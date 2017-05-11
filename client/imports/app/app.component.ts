44035fb59483ef3f88d93322a600f233bfce027c
Sou Lee <souleex@gmail.com>
2017-05-10 21:10:45 -0700

    finished chats page part

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+
 import { Platform } from 'ionic-angular';
+
 import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { ChatsPage } from '../pages/chats/chats';
+
 import template from "./app.html";
 
 @Component({
   template
 })
+@Component({
+
+  template
+
+})
+
 export class MyApp {
+
+  rootPage = ChatsPage;
   constructor(platform: Platform) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { MomentModule } from 'angular2-moment';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { ChatsPage } from '../pages/chats/chats'
+
 import { MyApp } from './app.component';
 
+ 
+
 @NgModule({
+
   declarations: [
-    MyApp
+
+    MyApp,
+
+    ChatsPage
+
   ],
+
+  ],
+
   imports: [
+
     IonicModule.forRoot(MyApp),
+
+    MomentModule
+
   ],
+
   bootstrap: [IonicApp],
+
   entryComponents: [
-    MyApp
+
+    MyApp,
+
+    ChatsPage
+
   ],
+
   providers: [
+
     { provide: ErrorHandler, useClass: IonicErrorHandler }
-  ]
-})
-export class AppModule {}
