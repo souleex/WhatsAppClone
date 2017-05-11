b5e54f3080bc30b293597784fe4fe6ada223918c
Sou Lee <souleex@gmail.com>
2017-05-10 21:36:27 -0700

    finished authentication section

--- server/methods.ts
+++ server/methods.ts
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Chats, Messages } from '../imports/collections';
-import { MessageType } from '../imports/models';
+import { MessageType, Profile } from '../imports/models';
 import { check, Match } from 'meteor/check';
 
 const nonEmptyString = Match.Where((str) => {
@@ -9,7 +9,23 @@ const nonEmptyString = Match.Where((str) => {
 });
 
 Meteor.methods({
+  updateProfile(profile: Profile): void {
+    if (!this.userId) throw new Meteor.Error('unauthorized',
+      'User must be logged-in to create a new chat');
+
+    check(profile, {
+      name: nonEmptyString
+    });
+
+    Meteor.users.update(this.userId, {
+      $set: {profile}
+    });
+  },
+
   addMessage(type: MessageType, chatId: string, content: string) {
+    if (!this.userId) throw new Meteor.Error('unauthorized',
+      'User must be logged-in to create a new chat');
+
     check(type, Match.OneOf(String, [ MessageType.TEXT ]));
     check(chatId, nonEmptyString);
     check(content, nonEmptyString);
@@ -24,6 +40,7 @@ Meteor.methods({
     return {
       messageId: Messages.collection.insert({
         chatId: chatId,
+        senderId: this.userId,
         content: content,
         createdAt: new Date(),
         type: type
